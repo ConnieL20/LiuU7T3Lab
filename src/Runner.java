14ecb7f1278ab730137af7948caf9f9fe9469430
student <sample@bths.edu>
2023-02-02 10:56:31 -0500

    2/2/23

---
+++ src/Runner.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Runner {
+    public static void main (String[] args) {
+//        ArrayList<String> names = new ArrayList<String>();
+//        names.add("Jamal");
+//        names.add("Emily");
+//        names.add("Destiny");
+//        names.add("Mateo");
+//
+//        //COMPLETE ME: print each name using an index based for loop
+//        for (int i = 0; i < names.size(); i++) {
+//            System.out.println(names.get(i));
+//
+//        }
+//        //COMPLETE ME: print each name (again) using enhanced for loop
+//        for (String name : names) {
+//            System.out.println(name);
+//        }
+//        //COMPLETE ME: print each name (a third time) using a while loop
+//        int i = 0;
+//        while (i < names.size()) {
+//            System.out.println(names.get(i));
+//            i++;
+//        }
+//
+//        ArrayList<Cat> catList = new ArrayList<Cat>();
+//        catList.add(new Cat("Patty"));
+//        catList.add(new Cat("Keith"));
+//        catList.add(new Cat("Jerry"));
+//        catList.add(new Cat("Mary"));
+//
+//        // COMPLETE ME: print each Cat’s name using a
+//        // standard for loop
+//        for (int j = 0; j < catList.size(); j++) {
+//            System.out.println(catList.get(j).getName());
+//        }
+//        // COMPLETE ME: print each Cat’s name using an
+//        // enhanced for loop
+//        for (Cat cat : catList) {
+//            System.out.println(cat.getName());
+//        }
+
+//        // create ArrayList from a list of initial values; requires Arrays.asList
+//        ArrayList<Integer> numList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9, 10, 11));
+//
+//        //COMPLETE ME!
+//        int newVal = 0;
+//        for (int i = 1; i < numList.size() - 2; i++) {
+//            if (numList.get(i - 1) % 2 == 0){
+//                newVal = numList.get(i + 1) + numList.get(i + 2);
+//                numList.set(i, newVal);
+//            }
+//        }
+//        System.out.println(numList);
+//
+//        ArrayList<Integer> ints = new ArrayList<Integer>();
+//        ints.add(0, 1);
+//        ints.add(0, 2);
+//        ints.add(0, 3);
+//        ints.add(0, 4);
+//        ints.add(0, 5);
+//        ints.add(0, 6);
+//
+//        ArrayList<Integer> newInts = new ArrayList<Integer>();
+//
+//        for (int i = ints.size() - 1; i >= 0; i--) {
+//            newInts.add(ints.get(i));
+//        }
+//
+//        System.out.println(ints);
+//        System.out.println(newInts);
+        Digits d1 = new Digits(15704);
+        Digits d2 = new Digits(0);
+
+    }
+}
+
+
+
+
+
+
